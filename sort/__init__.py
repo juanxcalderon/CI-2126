2457849c24a17241f6151a63b9746135f837a605
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2023-10-01 18:31:48 -0400

    Update __init__.py

--- sort/__init__.py
+++ sort/__init__.py
@@ -4,26 +4,26 @@ from re import findall
 
 @check50.check()
 def exists():
-    """answers.txt exists"""
+    """answers.txt existe"""
     check50.exists("answers.txt")
 
 @check50.check(exists)
 def answers():
-    """answers all questions"""
+    """responde todas las preguntas"""
     content = open("answers.txt", "r").read()
     if "TODO" in content:
-        raise check50.Failure("Not all questions answered.")
+        raise check50.Failure("No todas las preguntas son respondidas.")
 
 @check50.check(exists)
 def sorts():
-    """correctly identifies each sort"""
+    """identifica correctamente cada ordenación"""
 
-    check50.log("checking that sorts are classified correctly...")
+    check50.log("comprobando que los tipos están clasificados correctamente...")
 
     expected = ["sort1 uses:\s*[Bb][Uu][Bb][Bb][Ll][Ee]", "sort2 uses:\s*[Mm][Ee][Rr][Gg][Ee]", "sort3 uses:\s*[Ss][Ee][Ll][Ee][Cc][Tt][Ii][Oo][Nn]"]
     actual = open("answers.txt", "r").read()
 
     for e in expected:
         if not search(e, actual):
-            raise check50.Failure("Incorrect assignment of sorts.")
+            raise check50.Failure("Asignación incorrecta de ordenamientos.")
 
