5ecd4c130aa7592025832e35c421753a636d37d2
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2023-10-01 12:05:49 -0400

    Update __init__.py

--- mario/less/__init__.py
+++ mario/less/__init__.py
@@ -3,57 +3,57 @@ import check50.c
 
 @check50.check()
 def exists():
-    """mario.c exists"""
+    """mario.c existe"""
     check50.exists("mario.c")
     check50.include("1.txt", "2.txt", "8.txt")
 
 @check50.check(exists)
 def compiles():
-    """mario.c compiles"""
+    """mario.c compila"""
     check50.c.compile("mario.c", lcs50=True)
 
 @check50.check(compiles)
 def test_reject_negative():
-    """rejects a height of -1"""
+    """rechaza una altura de -1"""
     check50.run("./mario").stdin("-1").reject()
 
 @check50.check(compiles)
 def test0():
-    """rejects a height of 0"""
+    """rechaza una altura de 0"""
     check50.run("./mario").stdin("0").reject()
 
 @check50.check(compiles)
 def test1():
-    """handles a height of 1 correctly"""
+    """maneja la altura de 1 de forma correcta"""
     out = check50.run("./mario").stdin("1").stdout()
     check_pyramid(out, open("1.txt").read())
 
 @check50.check(compiles)
 def test2():
-    """handles a height of 2 correctly"""
+    """maneja la altura de 2 de forma correcta"""
     out = check50.run("./mario").stdin("2").stdout()
     check_pyramid(out, open("2.txt").read())
 
 @check50.check(compiles)
 def test8():
-    """handles a height of 8 correctly"""
+    """maneja la altura de 8 de forma correcta"""
     out = check50.run("./mario").stdin("8").stdout()
     check_pyramid(out, open("8.txt").read())
 
 @check50.check(compiles)
 def test9():
-    """rejects a height of -1, and then accepts a height of 2"""
-    out = check50.run("./mario").stdin("-1").reject().stdin("2").stdout()
-    check_pyramid(out, open("2.txt").read())
+    """rechaza la altura de -1, y después acepta la altura de 3"""
+    out = check50.run("./mario").stdin("-1").reject().stdin("3").stdout()
+    check_pyramid(out, open("3.txt").read())
 
 @check50.check(compiles)
 def test_reject_foo():
-    """rejects a non-numeric height of "foo" """
-    check50.run("./mario").stdin("foo").reject()
+    """rechaza una altura no numérica de "dfghdf" """
+    check50.run("./mario").stdin("dfghdf").reject()
 
 @check50.check(compiles)
 def test_reject_empty():
-    """rejects a non-numeric height of "" """
+    """rechaza una altura vacía "" """
     check50.run("./mario").stdin("").reject()
 
 
@@ -67,8 +67,8 @@ def check_pyramid(output, correct):
     help = None
     if len(output) == len(correct):
         if all(ol.rstrip() == cl for ol, cl in zip(output, correct)):
-            help = "did you add too much trailing whitespace to the end of your pyramid?"
+            help = "¿Agregaste demasiados espacios en blanco al final de tu pirámide?"
         elif all(ol[1:] == cl for ol, cl in zip(output, correct)):
-            help = "are you printing an additional character at the beginning of each line?"
+            help = "¿Estás imprimiendo un carácter adicional al principio de cada línea?"
 
     raise check50.Mismatch(correct, output, help=help)
