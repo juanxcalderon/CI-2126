3cf372d67c0baa46bed0a6b8e82817fa890f8eb1
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2023-10-01 16:06:28 -0400

    Update __init__.py

--- fiftyville/__init__.py
+++ fiftyville/__init__.py
@@ -5,20 +5,20 @@ import check50
 
 @check50.check()
 def exists():
-    """log.sql and answers.txt exist"""
+    """log.sql y answers.txt existen"""
     check50.exists("log.sql", "answers.txt")
 
 @check50.check(exists)
 def log_file():
-    """log file contains SELECT queries"""
+    """log file contiene consultas SELECT"""
 
     log = open("log.sql").read().lower()
     if "select" not in log:
-        raise check50.Failure(f"missing SELECT queries in log.sql")
+        raise check50.Failure(f"faltan consultas SELECT en log.sql")
 
 @check50.check(exists)
 def solved():
-    """mystery solved"""
+    """misterio resuelto"""
 
     answers = open("answers.txt").read().lower()
 
@@ -26,12 +26,12 @@ def solved():
     city = "6e657720796f726b"
     accomplice = "726f62696e"
 
-    for q in ["thief is", "escaped to", "accomplice is"]:
+    for q in ["ladrón es", "escapó a", "cómplice es"]:
         if answers.count(q) > 1:
-            raise check50.Failure("invalid answers.txt formatting")
+            raise check50.Failure("formato de answers.txt no válido")
 
-    identify_thief = re.search(f"thief\s*is\s*:?\s*{bytes.fromhex(thief).decode('utf-8')}", answers)
-    identify_city = re.search(f"escaped\s*to\s*:?\s*{bytes.fromhex(city).decode('utf-8')}", answers)
-    identify_accomplice = re.search(f"accomplice\s*is\s*:?\s*{bytes.fromhex(accomplice).decode('utf-8')}", answers)
+    identify_thief = re.search(f"ladrón\s*es\s*:?\s*{bytes.fromhex(thief).decode('utf-8')}", answers)
+    identify_city = re.search(f"escapó\s*a\s*:?\s*{bytes.fromhex(city).decode('utf-8')}", answers)
+    identify_accomplice = re.search(f"cómplice\s*es\s*:?\s*{bytes.fromhex(accomplice).decode('utf-8')}", answers)
     if not identify_thief or not identify_city or not identify_accomplice:
-        raise check50.Failure(f"answers.txt does not correctly solve mystery")
+        raise check50.Failure(f"answers.txt no resuelve correctamente el misterio")
