5b273cc5d409ecdf5ee3f4f0b4f2b642b54a1005
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2023-10-01 17:34:12 -0400

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -3,17 +3,17 @@ import check50
 
 @check50.check()
 def exists():
-    """README.md exists"""
+    """README.md existe"""
     check50.exists("README.md")
 
 @check50.check(exists)
 def final():
-    """final project details"""
+    """detalles del proyecto final"""
     text = open("README.md").read().lower()
     if len(text) < 2500:
-        raise check50.Failure(f"Description is not long enough.")
+        raise check50.Failure(f"La descripción no es lo suficientemente larga.")
        
     urls = re.findall('https?:\/\/[\w/\-?=%.]+\.[\w/\-?=%.]+', text)
     if not urls:
-        raise check50.Failure(f"Video URL is missing.")
+        raise check50.Failure(f"El URL del video no se encuentra.")
         
