2889ed384379b60895ecf4d3e0d6cd01ac5a22e0
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2023-10-01 14:40:17 -0400

    Update __init__.py

--- bulbs/__init__.py
+++ bulbs/__init__.py
@@ -4,59 +4,59 @@ import check50.c
 
 @check50.check()
 def exists():
-    """bulbs.c exists"""
+    """bulbs.c existe"""
     check50.exists("bulbs.c")
 
 
 @check50.check(exists)
 def compiles():
-    """bulbs.c compiles"""
+    """bulbs.c compila"""
     check50.c.compile("bulbs.c", lcs50=True)
 
 
 @check50.check(compiles)
 def bulbs_empty():
-    """bulbifies an empty message correctly"""
+    """bulbifica correctamente un mensaje vacío"""
     check50.run("./bulbs").stdin("").stdout("").exit(0)
     
 
 @check50.check(compiles)
 def bulbs_single_letter():
-    """bulbifies \"I\" correctly"""
+    """bulbifica \"I\" correctamente"""
     check50.run("./bulbs").stdin("I").stdout("⚫🟡⚫⚫🟡⚫⚫🟡\n").exit(0)
     
 
 @check50.check(compiles)
 def bulbs_multiple_letters():
-    """bulbifies \"xyz\" correctly"""
+    """bulbifica \"xyz\" correctamente"""
     check50.run("./bulbs").stdin("xyz").stdout("⚫🟡🟡🟡🟡⚫⚫⚫\n⚫🟡🟡🟡🟡⚫⚫🟡\n⚫🟡🟡🟡🟡⚫🟡⚫\n").exit(0)
 
 
 @check50.check(compiles)
 def bulbs_non_alpha():
-    """bulbifies \"?\" correctly"""
+    """bulbifica \"?\" correctamente"""
     check50.run("./bulbs").stdin("?").stdout("⚫⚫🟡🟡🟡🟡🟡🟡\n").exit(0)
 
 
 @check50.check(compiles)
 def bulbs_message():
-    """bulbifies \"Hi!\" correctly"""
+    """bulbifica \"Hi!\" correctamente"""
     check50.run("./bulbs").stdin("Hi!").stdout("⚫🟡⚫⚫🟡⚫⚫⚫\n⚫🟡🟡⚫🟡⚫⚫🟡\n⚫⚫🟡⚫⚫⚫⚫🟡\n").exit(0)
     
 
 @check50.check(compiles)
 def bulbs_mixed_case_alpha():
-    """bulbifies \"aBcDeFgHiJkLmNoPqRsTuVwXyZ\" correctly"""
+    """bulbifica \"aBcDeFgHiJkLmNoPqRsTuVwXyZ\" correctamente"""
     check50.run("./bulbs").stdin("aBcDeFgHiJkLmNoPqRsTuVwXyZ").stdout("⚫🟡🟡⚫⚫⚫⚫🟡\n⚫🟡⚫⚫⚫⚫🟡⚫\n⚫🟡🟡⚫⚫⚫🟡🟡\n⚫🟡⚫⚫⚫🟡⚫⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡⚫⚫⚫🟡🟡⚫\n⚫🟡🟡⚫⚫🟡🟡🟡\n⚫🟡⚫⚫🟡⚫⚫⚫\n⚫🟡🟡⚫🟡⚫⚫🟡\n⚫🟡⚫⚫🟡⚫🟡⚫\n⚫🟡🟡⚫🟡⚫🟡🟡\n⚫🟡⚫⚫🟡🟡⚫⚫\n⚫🟡🟡⚫🟡🟡⚫🟡\n⚫🟡⚫⚫🟡🟡🟡⚫\n⚫🟡🟡⚫🟡🟡🟡🟡\n⚫🟡⚫🟡⚫⚫⚫⚫\n⚫🟡🟡🟡⚫⚫⚫🟡\n⚫🟡⚫🟡⚫⚫🟡⚫\n⚫🟡🟡🟡⚫⚫🟡🟡\n⚫🟡⚫🟡⚫🟡⚫⚫\n⚫🟡🟡🟡⚫🟡⚫🟡\n⚫🟡⚫🟡⚫🟡🟡⚫\n⚫🟡🟡🟡⚫🟡🟡🟡\n⚫🟡⚫🟡🟡⚫⚫⚫\n⚫🟡🟡🟡🟡⚫⚫🟡\n⚫🟡⚫🟡🟡⚫🟡⚫\n").exit(0)
 
 
 @check50.check(compiles)
 def bulbs_spaces():
-    """bulbifies \" CS50 :) \" correctly"""
+    """bulbifica \" CS50 :) \" correctamente"""
     check50.run("./bulbs").stdin(" CS50 :) ").stdout("⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡⚫⚫⚫⚫🟡🟡\n⚫🟡⚫🟡⚫⚫🟡🟡\n⚫⚫🟡🟡⚫🟡⚫🟡\n⚫⚫🟡🟡⚫⚫⚫⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫⚫🟡🟡🟡⚫🟡⚫\n⚫⚫🟡⚫🟡⚫⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n").exit(0)
 
 
 @check50.check(compiles)
 def bulbs_finale():
-    """bulbifies The Great Gatsby's first sentence correctly"""
+    """Bulbifica correctamente la primera frase de The Great Gatsby"""
     check50.run("./bulbs").stdin("In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since.").stdout("⚫🟡⚫⚫🟡⚫⚫🟡\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫🟡🟡⚫🟡\n⚫🟡🟡🟡🟡⚫⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡🟡🟡⚫⚫🟡\n⚫🟡🟡⚫🟡🟡🟡🟡\n⚫🟡🟡🟡⚫🟡⚫🟡\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫🟡🟡⚫⚫🟡🟡🟡\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡🟡⚫⚫🟡⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫⚫⚫⚫🟡\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫🟡🟡⚫⚫🟡⚫⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫🟡🟡⚫🟡\n⚫🟡🟡⚫🟡🟡🟡🟡\n⚫🟡🟡🟡⚫⚫🟡⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡🟡⚫🟡🟡⚫\n⚫🟡🟡🟡⚫🟡⚫🟡\n⚫🟡🟡⚫🟡🟡⚫⚫\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡🟡⚫⚫🟡⚫\n⚫🟡🟡⚫⚫⚫⚫🟡\n⚫🟡🟡⚫⚫⚫🟡⚫\n⚫🟡🟡⚫🟡🟡⚫⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡🟡🟡⚫⚫🟡\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡⚫⚫⚫⚫🟡\n⚫🟡🟡🟡⚫⚫🟡⚫\n⚫🟡🟡🟡⚫⚫🟡🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫🟡🟡⚫🟡\n⚫🟡🟡🟡🟡⚫⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫⚫🟡🟡⚫\n⚫🟡🟡⚫⚫⚫⚫🟡\n⚫🟡🟡🟡⚫🟡⚫⚫\n⚫🟡🟡⚫🟡⚫⚫⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡🟡⚫⚫🟡⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫⚫🟡🟡🟡\n⚫🟡🟡⚫⚫⚫⚫🟡\n⚫🟡🟡🟡⚫🟡🟡⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫🟡🟡⚫🟡\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡🟡⚫⚫🟡🟡\n⚫🟡🟡⚫🟡🟡🟡🟡\n⚫🟡🟡⚫🟡🟡⚫🟡\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫⚫⚫⚫🟡\n⚫🟡🟡⚫⚫🟡⚫⚫\n⚫🟡🟡🟡⚫🟡🟡⚫\n⚫🟡🟡⚫🟡⚫⚫🟡\n⚫🟡🟡⚫⚫⚫🟡🟡\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡🟡⚫🟡⚫⚫\n⚫🟡🟡⚫🟡⚫⚫⚫\n⚫🟡🟡⚫⚫⚫⚫🟡\n⚫🟡🟡🟡⚫🟡⚫⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡⚫⚫🟡⚫⚫🟡\n⚫⚫🟡⚫⚫🟡🟡🟡\n⚫🟡🟡🟡⚫🟡🟡⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫⚫⚫🟡⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡🟡⚫🟡⚫⚫\n⚫🟡🟡🟡⚫🟡⚫🟡\n⚫🟡🟡🟡⚫⚫🟡⚫\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫🟡🟡⚫🟡⚫⚫🟡\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫🟡🟡⚫⚫🟡🟡🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫🟡🟡🟡🟡\n⚫🟡🟡🟡⚫🟡🟡⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡🟡⚫⚫🟡⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫🟡⚫⚫🟡\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫🟡🟡⚫🟡\n⚫🟡🟡🟡🟡⚫⚫🟡\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫🟡🟡⚫🟡\n⚫🟡🟡⚫🟡⚫⚫🟡\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫🟡🟡⚫⚫🟡⚫⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡🟡⚫🟡🟡⚫\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫🟡🟡🟡⚫⚫🟡⚫\n⚫⚫🟡⚫⚫⚫⚫⚫\n⚫🟡🟡🟡⚫⚫🟡🟡\n⚫🟡🟡⚫🟡⚫⚫🟡\n⚫🟡🟡⚫🟡🟡🟡⚫\n⚫🟡🟡⚫⚫⚫🟡🟡\n⚫🟡🟡⚫⚫🟡⚫🟡\n⚫⚫🟡⚫🟡🟡🟡⚫\n").exit(0)
