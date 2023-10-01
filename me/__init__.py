6f0bdd04aed26895f6aa07431ac03357d98494b9
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2023-10-01 17:03:54 -0400

    Update __init__.py

--- me/__init__.py
+++ me/__init__.py
@@ -3,25 +3,25 @@ import check50.c
 
 @check50.check()
 def exists():
-    """hello.c exists"""
+    """hello.c existe"""
     check50.exists("hello.c")
 
 @check50.check(exists)
 def compiles():
-    """hello.c compiles"""
+    """hello.c compila"""
     check50.c.compile("hello.c", lcs50=True)
 
 @check50.check(compiles)
 def emma():
-    """responds to name Emma"""
+    """responde al nombre de Emma"""
     check50.run("./hello").stdin("Emma").stdout("Emma").exit()
 
 @check50.check(compiles)
 def inno():
-    """responds to name Inno"""
+    """responde al nombre de Inno"""
     check50.run("./hello").stdin("Inno").stdout("Inno").exit()
 
 @check50.check(compiles)
 def kamryn():
-    """responds to name Kamryn"""
+    """responde al nombre de Kamryn"""
     check50.run("./hello").stdin("Kamryn").stdout("Kamryn").exit()
