3b5f3aa326e61d188b3c5fa10629f4f1829a350f
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2023-10-01 19:03:47 -0400

    Update __init__.py

--- volume/__init__.py
+++ volume/__init__.py
@@ -5,33 +5,33 @@ HASHES = ["268f2deee976fcc8dcc915be63cd3d4ac003a73a4b8bfd6f7b95c441a42ed1ec", "4
 
 @check50.check()
 def exists():
-    """volume.c exists"""
+    """volume.c existe"""
     check50.exists("volume.c")
     check50.include("input.wav")
 
 @check50.check(exists)
 def compiles():
-    """volume.c compiles"""
+    """volume.c compila"""
     check50.c.compile("volume.c", lcs50=True)
 
 @check50.check(compiles)
 def audio_half():
-    """reduces audio volume, factor of 0.5 correctly"""
+    """reduce el volumen de audio, factor de 0,5 correctamente"""
     check50.run("./volume input.wav output.wav 0.5").exit(0)
     if check50.hash("output.wav") != HASHES[0]:
-        raise check50.Failure("audio is not correctly altered, factor of 0.5")
+        raise check50.Failure("El audio no está alterado correctamente, factor de 0,5")
 
 @check50.check(compiles)
 def audio_tenth():
-    """reduces audio volume, factor of 0.1 correctly"""
+    """reduce el volumen de audio, factor de 0,1 correctamente"""
     check50.run("./volume input.wav output.wav 0.1").exit(0)
     if check50.hash("output.wav") != HASHES[1]:
-        raise check50.Failure("audio is not correctly altered, factor of 0.1")
+        raise check50.Failure("El audio no está alterado correctamente, factor de 0,1")
 
 @check50.check(compiles)
 def audio_x2():
-    """increases audio volume, factor of 2 correctly"""
+    """aumenta el volumen de audio, factor de 2 correctamente"""
     check50.run("./volume input.wav output.wav 2").exit(0)
     if check50.hash("output.wav") != HASHES[2]:
-        raise check50.Failure("audio is not correctly altered, factor of 2")
+        raise check50.Failure("El audio no está alterado correctamente, factor de 2")
 
