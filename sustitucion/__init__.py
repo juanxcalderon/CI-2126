ae2118c219a95477c5bd6f08d494ae5124ecd0ae
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2024-05-19 20:18:58 -0400

    Update __init__.py
    Colocando pruebas en castellano

--- sustitucion/__init__.py
+++ sustitucion/__init__.py
@@ -17,55 +17,55 @@ def compiles():
 @check50.check(compiles)
 def encrypt1():
     """encripta de la "A" a la "Z" usando ZYXWVUTSRQPONMLKJIHGFEDCBA como clave"""
-    check50.run("./substitution ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("A").stdout("ciphertext:\s*Z\n", "ciphertext: Z\n").exit(0)
+    check50.run("./substitution ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("A").stdout("texto encriptado:\s*Z\n", "texto encriptado: Z\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt2():
     """encripta de la "a" a la "z" usando ZYXWVUTSRQPONMLKJIHGFEDCBA como clave"""
-    check50.run("./substitution ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("a").stdout("ciphertext:\s*z\n", "ciphertext: z\n").exit(0)
+    check50.run("./substitution ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("a").stdout("texto encriptado:\s*z\n", "texto encriptado: z\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt3():
     """encripta "ABC" como "NJQ" usando NJQSUYBRXMOPFTHZVAWCGILKED como clave"""
-    check50.run("./substitution NJQSUYBRXMOPFTHZVAWCGILKED").stdin("ABC").stdout("ciphertext:\s*NJQ\n", "ciphertext: NJQ\n").exit(0)
+    check50.run("./substitution NJQSUYBRXMOPFTHZVAWCGILKED").stdin("ABC").stdout("texto encriptado:\s*NJQ\n", "texto encriptado: NJQ\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt4():
     """encripta "XyZ" como "KeD" usando NJQSUYBRXMOPFTHZVAWCGILKED como clave"""
-    check50.run("./substitution NJQSUYBRXMOPFTHZVAWCGILKED").stdin("XyZ").stdout("ciphertext:\s*KeD\n", "ciphertext: KeD\n").exit(0)
+    check50.run("./substitution NJQSUYBRXMOPFTHZVAWCGILKED").stdin("XyZ").stdout("texto encriptado:\s*KeD\n", "texto encriptado: KeD\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt5():
     """encripta "This is CS50" como "Cbah ah KH50" usando YUKFRNLBAVMWZTEOGXHCIPJSQD as clave"""
-    check50.run("./substitution YUKFRNLBAVMWZTEOGXHCIPJSQD").stdin("This is CS50").stdout("ciphertext:\s*Cbah ah KH50\n", "ciphertext: Cbah ah KH50\n").exit(0)
+    check50.run("./substitution YUKFRNLBAVMWZTEOGXHCIPJSQD").stdin("This is CS50").stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt6():
     """encripta "This is CS50" como "Cbah ah KH50" usando yukfrnlbavmwzteogxhcipjsqd como clave"""
-    check50.run("./substitution yukfrnlbavmwzteogxhcipjsqd").stdin("This is CS50").stdout("ciphertext:\s*Cbah ah KH50\n", "ciphertext: Cbah ah KH50\n").exit(0)
+    check50.run("./substitution yukfrnlbavmwzteogxhcipjsqd").stdin("This is CS50").stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt7():
     """encripta "This is CS50" como "Cbah ah KH50" usando YUKFRNLBAVMWZteogxhcipjsqd como clave"""
-    check50.run("./substitution YUKFRNLBAVMWZteogxhcipjsqd").stdin("This is CS50").stdout("ciphertext:\s*Cbah ah KH50\n", "ciphertext: Cbah ah KH50\n").exit(0)
+    check50.run("./substitution YUKFRNLBAVMWZteogxhcipjsqd").stdin("This is CS50").stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt8():
     """encripta todos los caracteres alfabéticos usando DWUSXNPQKEGCZFJBTLYROHIAVM como clave"""
-    check50.run("./substitution DWUSXNPQKEGCZFJBTLYROHIAVM").stdin("The quick brown fox jumps over the lazy dog").stdout("ciphertext:\s*Rqx tokug wljif nja eozby jhxl rqx cdmv sjp\n", "ciphertext: Rqx tokug wljif nja eozby jhxl rqx cdmv sjp\n").exit(0)
+    check50.run("./substitution DWUSXNPQKEGCZFJBTLYROHIAVM").stdin("The quick brown fox jumps over the lazy dog").stdout("texto encriptado:\s*Rqx tokug wljif nja eozby jhxl rqx cdmv sjp\n", "texto encriptado: Rqx tokug wljif nja eozby jhxl rqx cdmv sjp\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt9():
     """no cifra caracteres no-alfabéticos utilizando DWUSXNPQKEGCZFJBTLYROHIAVM como clave"""
-    check50.run("./substitution DWUSXNPQKEGCZFJBTLYROHIAVM").stdin("Shh... Don't tell!").stdout("ciphertext:\s*Yqq... Sjf'r rxcc!\n", "ciphertext: Yqq... Sjf'r rxcc!\n").exit(0)
+    check50.run("./substitution DWUSXNPQKEGCZFJBTLYROHIAVM").stdin("Shh... Don't tell!").stdout("texto encriptado:\s*Yqq... Sjf'r rxcc!\n", "texto encriptado: Yqq... Sjf'r rxcc!\n").exit(0)
 
 
 @check50.check(compiles)
