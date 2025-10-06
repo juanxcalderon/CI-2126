73a824bdfe7bfe18744be71a542a20b457569cc6
juanxcalderon <jcalderon@usb.ve>
2025-10-06 12:25:20 -0400

    Update __init__.py

--- cifrado/__init__.py
+++ cifrado/__init__.py
@@ -17,55 +17,78 @@ def compiles():
 @check50.check(compiles)
 def encrypt1():
     """encripta de la "A" a la "Z" usando ZYXWVUTSRQPONMLKJIHGFEDCBA como clave"""
-    check50.run("./cifrado ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("A").stdout("texto encriptado:\s*Z\n", "texto encriptado: Z\n").exit(0)
+    check50.run("./cifrado ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("A").stdout(
+        "texto encriptado:\s*Z\n", "texto encriptado: Z\n"
+    ).exit(0)
 
 
 @check50.check(compiles)
 def encrypt2():
     """encripta de la "a" a la "z" usando ZYXWVUTSRQPONMLKJIHGFEDCBA como clave"""
-    check50.run("./cifrado ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("a").stdout("texto encriptado:\s*z\n", "texto encriptado: z\n").exit(0)
+    check50.run("./cifrado ZYXWVUTSRQPONMLKJIHGFEDCBA").stdin("a").stdout(
+        "texto encriptado:\s*z\n", "texto encriptado: z\n"
+    ).exit(0)
 
 
 @check50.check(compiles)
 def encrypt3():
     """encripta "ABC" como "NJQ" usando NJQSUYBRXMOPFTHZVAWCGILKED como clave"""
-    check50.run("./cifrado NJQSUYBRXMOPFTHZVAWCGILKED").stdin("ABC").stdout("texto encriptado:\s*NJQ\n", "texto encriptado: NJQ\n").exit(0)
+    check50.run("./cifrado NJQSUYBRXMOPFTHZVAWCGILKED").stdin("ABC").stdout(
+        "texto encriptado:\s*NJQ\n", "texto encriptado: NJQ\n"
+    ).exit(0)
 
 
 @check50.check(compiles)
 def encrypt4():
     """encripta "XyZ" como "KeD" usando NJQSUYBRXMOPFTHZVAWCGILKED como clave"""
-    check50.run("./cifrado NJQSUYBRXMOPFTHZVAWCGILKED").stdin("XyZ").stdout("texto encriptado:\s*KeD\n", "texto encriptado: KeD\n").exit(0)
+    check50.run("./cifrado NJQSUYBRXMOPFTHZVAWCGILKED").stdin("XyZ").stdout(
+        "texto encriptado:\s*KeD\n", "texto encriptado: KeD\n"
+    ).exit(0)
 
 
 @check50.check(compiles)
 def encrypt5():
     """encripta "This is CS50" como "Cbah ah KH50" usando YUKFRNLBAVMWZTEOGXHCIPJSQD as clave"""
-    check50.run("./cifrado YUKFRNLBAVMWZTEOGXHCIPJSQD").stdin("This is CS50").stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
+    check50.run("./cifrado YUKFRNLBAVMWZTEOGXHCIPJSQD").stdin(
+        "This is CS50"
+    ).stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt6():
     """encripta "This is CS50" como "Cbah ah KH50" usando yukfrnlbavmwzteogxhcipjsqd como clave"""
-    check50.run("./cifrado yukfrnlbavmwzteogxhcipjsqd").stdin("This is CS50").stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
+    check50.run("./cifrado yukfrnlbavmwzteogxhcipjsqd").stdin(
+        "This is CS50"
+    ).stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt7():
     """encripta "This is CS50" como "Cbah ah KH50" usando YUKFRNLBAVMWZteogxhcipjsqd como clave"""
-    check50.run("./cifrado YUKFRNLBAVMWZteogxhcipjsqd").stdin("This is CS50").stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
+    check50.run("./cifrado YUKFRNLBAVMWZteogxhcipjsqd").stdin(
+        "This is CS50"
+    ).stdout("texto encriptado:\s*Cbah ah KH50\n", "texto encriptado: Cbah ah KH50\n").exit(0)
 
 
 @check50.check(compiles)
 def encrypt8():
     """encripta todos los caracteres alfabéticos usando DWUSXNPQKEGCZFJBTLYROHIAVM como clave"""
-    check50.run("./cifrado DWUSXNPQKEGCZFJBTLYROHIAVM").stdin("The quick brown fox jumps over the lazy dog").stdout("texto encriptado:\s*Rqx tokug wljif nja eozby jhxl rqx cdmv sjp\n", "texto encriptado: Rqx tokug wljif nja eozby jhxl rqx cdmv sjp\n").exit(0)
+    check50.run("./cifrado DWUSXNPQKEGCZFJBTLYROHIAVM").stdin(
+        "The quick brown fox jumps over the lazy dog"
+    ).stdout(
+        "texto encriptado:\s*Rqx tokug wljif nja eozby jhxl rqx cdmv sjp\n",
+        "texto encriptado: Rqx tokug wljif nja eozby jhxl rqx cdmv sjp\n",
+    ).exit(0)
 
 
 @check50.check(compiles)
 def encrypt9():
     """no cifra caracteres no-alfabéticos utilizando DWUSXNPQKEGCZFJBTLYROHIAVM como clave"""
-    check50.run("./cifrado DWUSXNPQKEGCZFJBTLYROHIAVM").stdin("Shh... Don't tell!").stdout("texto encriptado:\s*Yqq... Sjf'r rxcc!\n", "texto encriptado: Yqq... Sjf'r rxcc!\n").exit(0)
+    check50.run("./cifrado DWUSXNPQKEGCZFJBTLYROHIAVM").stdin(
+        "Shh... Don't tell!"
+    ).stdout(
+        "texto encriptado:\s*Yqq... Sjf'r rxcc!\n", "texto encriptado: Yqq... Sjf'r rxcc!\n"
+    ).exit(0)
 
 
 @check50.check(compiles)
@@ -108,3 +131,9 @@ def handles_duplicate_chars_lower():
 def handles_multiple_duplicate_chars():
     """maneja múltiples caracteres duplicados en la clave de encriptación"""
     check50.run("./cifrado MMCcEFGHIJKLMNOPqRqTUVWXeZ").exit(1)
+
+
+@check50.check(compiles)
+def handles_single_duplicate_character():
+    """maneja un carácter duplicado de mayúsculas y minúsculas en la clave."""
+    check50.run("./cifrado ABCDEFGHIJKLMNOPpQRSTUVWXY").exit(1)
